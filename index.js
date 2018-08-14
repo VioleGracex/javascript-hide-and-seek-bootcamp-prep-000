32a0f6b7f79aaf748c81c89485ad7dfd057704b0
Viole Grace <highdevilxx@hotmail.com>
2018-08-14 12:06:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+function getFirstSelector(selector)
+ {
+   return document.querySelector(selector);
+ }
+
+ function nestedTarget()
+ {
+   return document.querySelector('#nested .target');
+ }
+
+ function deepestChild()
+ {
+   var grandNode = document.querySelector('#grand-node');
+
+
+   return deepestNode;
+ }
+
+ function increaseRankBy(n)
+ {
+   var ranks =  document.querySelectorAll('.ranked-list');
+   for (var i = 0; i < ranks.length; i++)
+   {
+     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+   }
+ }
